620e37b0c8c38965956697102dd7f10784b8d20f
maryzarovska <122176941+maryzarovska@users.noreply.github.com>
2026-08-05 21:56:30 +0200

    Update database.js

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: 'postgresql://postgres:Mdls22113123@db.epcndhutstueavxkcrch.supabase.co:5432/postgres',
+    connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false }
 });
 
